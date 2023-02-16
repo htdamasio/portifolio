4aca9b5ac076c58d0fc3414e66406c2e0b1463a0
Henrique Tomé <henri.vini@hotmail.com>
2023-02-16 09:37:39 -0300

    Adding remix-run/vercel

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [
+    // "@apollo/client",
+    // "ts-invariant",
+    // "zen-observable-ts"
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
